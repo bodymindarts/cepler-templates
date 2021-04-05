eb52fc799166d015c4ee303f9cc9c5d10176c027
Long Nguyen <long.nguyen11288@gmail.com>
2021-04-05 08:49:31 -0400

    added support for tfvars and cleaned up example with correct paths

--- examples/concourse-terraform/terraform/null.tf
+++ examples/concourse-terraform/terraform/null.tf
@@ -1,2 +1,17 @@
+variable "env_name" {
+  type        = string
+  description = "Name of environment."
+}
+
+data "null_data_source" "values" {
+  inputs = {
+    env_name = var.env_name
+  }
+}
+
 resource "null_resource" "cluster" {
+}
+
+output "env_name" {
+  value = data.null_data_source.values.outputs["env_name"]
 }
--- templates/globals.star
+++ templates/globals.star
@@ -1,6 +1,6 @@
 globals = {
   "templates_version": "0.2.0",
-  "cepler_version": "0.4.8",
+  "cepler_version": "0.4.9",
   "concourse_task_image": "bodymindarts/cepler-templates",
-  "terraform_version": "latest"
+  "terraform_version": "0.14.9"
 }
